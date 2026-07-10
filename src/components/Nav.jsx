f360584603a280978c1efcab4f34d706b141963a
Bryan Luu <loc.bryan.luu@gmail.com>
2026-07-10 11:26:23 -0700

    :sparkles: active page now highlighted in Nav
    Navbar active class is addedd if the title matches the page label.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,22 @@
+import { build } from "astro";
 import React from "react";
+
+function buildNavLinks(activePage) {
+  const navLinks = {
+    "Home": "/index.html",
+    /* The link below won't work locally because it links to the resume repo */
+    "Resumé": "/resume/resume.pdf"
+  };
+
+  return Object.entries(navLinks).map(entry => {
+    const [title, link] = entry;
+    if (title === activePage)
+      return <li class="nav-item"><a href={link} class="nav-link active">{title}</a></li>;
+    else
+      return <li class="nav-item"><a href={link} class="nav-link">{title}</a></li>;
+  }
+  );
+}
 
 function Nav(props) {
   return <nav class="navbar navbar-dark bg-primary mb-4 border-bottom fixed-top">
@@ -7,9 +25,7 @@ function Nav(props) {
         <span class="fs-4">Bryan Luu</span>
       </a>
       <ul class="navbar-nav d-flex flex-row flex-fill gap-3 px-3">
-        <li class="nav-item"><a href="/index.html" class="nav-link">Home</a></li>
-        {/* The link below won't work locally because it links to the resume repo */}
-        <li class="nav-item"><a href="/resume/resume.pdf" class="nav-link">Resumé</a></li>
+        {buildNavLinks(props.activePage)}
       </ul>
       <button class="btn btn-primary">Book a call <i class="bi bi-telephone-fill"></i ></button>
     </div>

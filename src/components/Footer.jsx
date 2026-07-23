da03ef472f86439388c5e01ff2016607b6faf7af
Bryan Luu <loc.bryan.luu@gmail.com>
2026-07-23 15:41:12 -0700

    :pencil2: remove period in Copyright
    Period feels a bit serious unnecessarily.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer(props) {
 
   return (
     <footer className="d-flex justify-content-between p-3 align-items-center mt-auto border-top bg-secondary text-bg-secondary">
-      <span>© {currentYear} Bryan Luu.</span>
+      <span>© {currentYear} Bryan Luu</span>
       <ul className="d-flex flex-row navbar-nav gap-3">
         <li className="nav-item">
           <ExternalLink
